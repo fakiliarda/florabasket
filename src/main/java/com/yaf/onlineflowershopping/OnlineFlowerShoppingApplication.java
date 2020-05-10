84d348658017ffc66517d9e8ec1f0ee36729ca3f
Arda Fakılı <ardafakili@gmail.com>
2020-05-10 23:15:38 +0300

    initial commit

---
+++ src/main/java/com/yaf/onlineflowershopping/OnlineFlowerShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.yaf.onlineflowershopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFlowerShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFlowerShoppingApplication.class, args);
+	}
+
+}
